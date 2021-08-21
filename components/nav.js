fba422ef1b3712c288ad87ced98f9fda8f59240d
Jon <jprather.dev@gmail.com>
2021-08-20 23:42:21 -0700

    init basic nav bar

---
+++ components/nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function nav() {
+  return (
+    <nav className='nav'>
+      <p className='nav__brand-title'>
+        {" "}
+        <span>sno</span>When
+      </p>
+    </nav>
+  );
+}
