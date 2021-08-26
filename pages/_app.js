c9b4e84ef92c6cace679a3775f13166d674634ef
Jon <jprather.dev@gmail.com>
2021-08-25 23:17:55 -0700

    moved meta tags and title to app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { Hydrate } from "react-query/hydration";
 import { ReactQueryDevtools } from "react-query/devtools";
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   const twentyFourHoursInMs = 1000 * 60 * 60 * 24;
@@ -24,11 +25,28 @@ export default function MyApp({ Component, pageProps }) {
   //   const queryClient = new QueryClient();
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-      <ReactQueryDevtools />
-    </QueryClientProvider>
+    <>
+      <Head>
+        <title>SnoWhen</title>
+        <meta charset='UTF-8' />
+        <meta
+          name='description'
+          content='Snow Report for Resort Locations App'
+        />
+        <meta
+          name='keywords'
+          content='HTML, CSS, JavaScript,API, Snow, Resorts, California'
+        />
+        <meta name='author' content='Jon' />
+
+        <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      </Head>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+        <ReactQueryDevtools />
+      </QueryClientProvider>
+    </>
   );
 }
