7ae2b6cdedc78dda321c7a157f919292d747db26
Jon <jprather.dev@gmail.com>
2021-08-30 10:50:37 -0700

    made breadcrumbs for backButton

--- components/backButton.js
+++ components/backButton.js
@@ -2,12 +2,19 @@ import React from "react";
 import Link from "next/link";
 import { useRouter } from "next/router";
 
-export default function backButton({ url }) {
+export default function backButton({ url, path }) {
   const router = useRouter();
   return (
-    <button className='back-button' onClick={() => router.push(url)}>
-      <p className='back-button__lt'>{String.fromCharCode(60)}</p>
-      <p className='back-button__text'>Back</p>
-    </button>
+    <div className='back-button__wrapper'>
+      <button className='back-button__button' onClick={() => router.push(url)}>
+        <p className='back-button__button__lt'>{String.fromCharCode(60)}</p>
+        <p className='back-button__button__text'>Back</p>
+      </button>
+      <div className='back-button__path'>
+        {path?.map((ele, i) => {
+          return <span>/ {ele}</span>;
+        })}
+      </div>
+    </div>
   );
 }
