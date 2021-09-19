69445bdcf8085f8db425f46149793e798d06a91d
Jon <jprather.dev@gmail.com>
2021-09-18 18:46:17 -0700

    init nav redesign

--- components/nav.js
+++ components/nav.js
@@ -4,16 +4,29 @@ import DarkMode from "./darkMode";
 
 export default function nav() {
   return (
-    <>
+    <div className='nav__wrapper'>
       <nav className='nav'>
-        <Link href='/weather'>
-          <p className='nav__brand-title'>
-            {" "}
-            <span>Sno</span>When
-          </p>
-        </Link>
+        <div>
+          <div className='nav__pages-container'>
+            <Link href='/weather'>
+              <>
+                <img src='/logo.png' className='nav__brand-title' />
+                <p>SnoWhen</p>
+              </>
+            </Link>
+            <button className='nav__pages-container__home'>Home</button>
+            <button className='nav__pages-container__about'>About</button>
+
+            <button className='nav__pages-container__faq'>FAQ</button>
+          </div>
+        </div>
+        <div className='nav__auth-container'>
+          <button className='nav__auth-container__log-in'>Log in</button>
+          <button className='nav__auth-container__sign-up'>Sign up</button>
+        </div>
+        {/* <DarkMode /> */}
+        {/* <div className='nav__hr'></div> */}
       </nav>
-      <DarkMode />
-    </>
+    </div>
   );
 }
