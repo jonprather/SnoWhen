23938debb4502d70885187d74ca792a9e471f8ec
Jon <jprather.dev@gmail.com>
2021-11-02 16:18:10 -0700

    modularized location scss

--- pages/weather/[location]/index.js
+++ pages/weather/[location]/index.js
@@ -47,7 +47,7 @@ export default function location() {
       </div>
       <div className='location__forecast'>
         {!weatherObj && <p className='subheading'>..Loading</p>}
-        <div className='day__forecast__graph-container'>
+        <div className='location__forecast__graph-container'>
           {weatherObj && (
             <Graph
               location={location}
@@ -59,7 +59,7 @@ export default function location() {
         </div>
       </div>
       <div className='location__weather'>
-        <div className='day__weather__cards-container'>
+        <div className=''>
           <div className='location__header--2'>
             <h1 className='heading'>{location} </h1>
             <h2 className='subheading location__weather__subheading mb-18'>
