f3537e2fc0bcf297865251834a5bc8ad1e87e29e
Jon <jprather.dev@gmail.com>
2022-07-08 11:33:09 -0700

    changed invalidate queries to setQueryData

--- components/hooks/useSaveSearchHistory.js
+++ components/hooks/useSaveSearchHistory.js
@@ -19,15 +19,9 @@ const saveSearchHistory = async ({ resort }) => {
 
   const data = await res.json();
   if (res.ok) {
-    // so if want to do setQueryClient cache then need to get cache for a given key ( resorts)
-    //then update it
-    //but might be better to do that in useMutaiton or here or at call site whre? maybe onSuccess?
-    // const arrayCopy = searchHistory ? searchHistory.data.slice() : [];
-    // arrayCopy.push(data.data.data);
-    // setSearchHistory((favArrObjs) => {
-    //   return { ...favArrObjs, data: arrayCopy };
-    // });
-    // router.push("/account/");
+    console.log("add res resp", data);
+
+    return data;
   } else {
     // setError(data.message);
     // setError(null);
@@ -38,23 +32,19 @@ export default function useSaveSearchHistory() {
   const queryClient = useQueryClient();
 
   const { mutate } = useMutation(saveSearchHistory, {
-    onSuccess: () => queryClient.invalidateQueries([queryKeys.resorts]),
+    onSuccess: async (newResort) => {
+      queryClient.setQueryData(queryKeys.resorts, (old = []) => {
+        const oldArray = old.resortsSearchHistory.data;
+        const newArray = newResort.data.data;
+        const combindedArray = [...oldArray, newArray];
+        let tempObj = Object.assign({}, old);
+        tempObj.resortsSearchHistory.data = combindedArray;
+        return tempObj;
+      });
+    },
+
+    //() => queryClient.invalidateQueries([queryKeys.resorts]),
   });
-  // ok so i got confused its called data the feild i wanted to pull off
-  //taking off a needless wrapper object
+
   return mutate;
 }
-
-// // TODO: update return type
-// export function useCancelAppointment(): (appointment: Appointment) => void {
-//   const queryClient = useQueryClient();
-//   // const toast = useCustomToast();
-//   const { mutate } = useMutation(
-//     (appointment: Appointment) => removeAppointmentUser(appointment),
-//     {
-//       onSuccess: () => queryClient.invalidateQueries([queryKeys.appointments]),
-//     }
-//   );
-//   // TODO: replace with mutate function
-//   return mutate;
-// }
